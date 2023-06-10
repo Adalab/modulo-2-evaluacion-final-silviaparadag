f39422a57210b41d27e26944fd1a4f038ce62dad
Silvia Parada <silviaparadag@yahoo.es>
2023-06-10 11:00:55 +0200

    Show character list with fecth

--- src/js/main.js
+++ src/js/main.js
@@ -9,30 +9,40 @@ const cardList = document.querySelector('.js_list');
 // VARIABLES
 const serverURL = `https://api.disneyapi.dev/character?page=50`;
 
-
 let disneyDataList = [];
 
+
 //FETCH
+/* */
 fetch(serverURL)
   .then((response) => response.json())
   .then((listData) => {
     console.log(listData);
-    disneyDataList = listData.data[46];
+    disneyDataList = listData.data;
     console.log(disneyDataList.name);
-    cardList.innerHTML = renderOneCharacter(disneyDataList);
+    renderAllCharacters(disneyDataList);
   });
 
 
 // FUNCTIONS
+/* */
+function renderAllCharacters (list) {
+  for (const eachCharacter of list ) {
+    cardList.innerHTML += renderOneCharacter(eachCharacter);
+  }
+}
 
-function renderOneCharacter() {
-  let html = `<li id="${disneyDataList.__id}" class="card">
+
+function renderOneCharacter(disneyDataObj) {
+  let html = `<li id="${disneyDataObj.__id}" class="card">
                 <article class="character__box">
-                    <img class="character__img js_img" src="${disneyDataList.imageUrl}" alt="Disney Characters" />
-                    <p class="character__name js_name">${disneyDataList.name}</p>
+                <img class="character__img js_img" src="${disneyDataObj.imageUrl}" alt="Disney Characters" />
+                <p class="character__name js_name">${disneyDataObj.name}</p>
                 </article>
             </li>`;
   return html;
 }
 
+
+/* */
 // EVENTS
